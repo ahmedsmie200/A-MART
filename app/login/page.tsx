325959f8ba18b4b68f3c1bcb2a951462e853dde1
Ahmed Samir <your_email@example.com>
2026-02-17 14:01:42 +0200

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/LoginForm/LoginForm";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col justify-center items-center h-[60vh]">
+      <h1 className="text-2xl font-bold mb-4">Login</h1>
+      <LoginForm />
+    </div>
+  )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        port: "",
+        pathname: "/Route-Academy-*/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
